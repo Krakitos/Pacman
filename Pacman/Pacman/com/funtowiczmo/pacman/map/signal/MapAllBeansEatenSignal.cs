b79c45fdc4319499f84523c9c0eb441bc97165cf
Morgan Funtowicz <funtowiczmo@gmail.com>
2013-12-04 18:26:37 +0100

    Added event to inform all beans has been eaten

---
+++ Pacman/Pacman/com/funtowiczmo/pacman/map/signal/MapAllBeansEatenSignal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pacman.com.funtowiczmo.pacman.map.signal
+{
+    public class MapAllBeansEatenSignal : MapSignal
+    {
+        public MapAllBeansEatenSignal(Map map)
+            : base(map)
+        {
+
+        }
+    }
+}
