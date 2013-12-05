0f2959e313215213350863c2fffd1880d304a7f1
Morgan Funtowicz <funtowiczmo@gmail.com>
2013-12-05 11:39:57 +0100

    Movement aborting possibilities

--- Pacman/Pacman/Pacman.cs
+++ Pacman/Pacman/Pacman.cs
@@ -322,6 +322,7 @@ namespace Pacman
                     if (pacman.Direction != EntityDirectionEnum.BOTTOM)
                     {
                         pacman.Direction = EntityDirectionEnum.BOTTOM;
+                        pacman.AbortMovement();
                     }
                 }
             }
